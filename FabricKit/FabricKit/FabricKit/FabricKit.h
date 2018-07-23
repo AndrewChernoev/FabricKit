a49a18db72fe23904d8911d31f00fe9b1e5d7d99
Andrey <chernoev.wt@gmail.com>
2018-07-23 17:54:37 +0700

    Add project

---
+++ FabricKit/FabricKit/FabricKit/FabricKit.h
@@ -0,0 +1,19 @@
+//
+//  FabricKit.h
+//  FabricKit
+//
+//  Created by Chernoev Andrew on 23/07/2018.
+//  Copyright © 2018 AndrewChernoev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FabricKit.
+FOUNDATION_EXPORT double FabricKitVersionNumber;
+
+//! Project version string for FabricKit.
+FOUNDATION_EXPORT const unsigned char FabricKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FabricKit/PublicHeader.h>
+
+
